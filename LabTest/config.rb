8879881c35064fbad536ee688c4148dbbd06f478
Lion Hu <hulion1972@gmail.com>
2018-06-05 11:47:31 +0800

    增加Test項目
    增加Test項目

---
+++ LabTest/config.rb
@@ -0,0 +1,9 @@
+http_path = '../'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = false
+output_style = :compressed
+generated_images_dir = 'images/sprite'
